c08e78b190da0208ff42601bee8b6aa4f0b9bda0
nguyenquanghuy1999 <nguyenquanghuy1999@gmail.com>
2020-12-28 16:21:07 +0700

    Hoan thanh

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -10,7 +10,7 @@ export default class ProductList extends Component{
            <React.Fragment>
                <div className="py-5">
                 <div className="container" >
-                    <Title name="Các" title="Sản Phẩm Của Chúng Tôi" style="margin-left: 4em"/>
+                    <Title name="Các" title="Sản Phẩm Của Chúng Tôi"/>
                     <div className="row">
                         <ProductConsumer>
                             {value => {
